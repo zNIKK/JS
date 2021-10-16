a80e6d95bdc607ce0c39621f1e515a6ca0506a9d
zNIKK <nicolas.gandolfi11@gmail.com>
2021-10-16 03:23:43 -0300

    refeito projeto Que_horas_sao para treino

--- Basico/007_repetições.js/015.js
+++ Basico/007_repetições.js/015.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+let c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/* let c = 1
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+
+
+/* 
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem') 
+*/
---
+++ Basico/007_repetições.js/016.js
@@ -0,0 +1,14 @@
+
+/*
+let c = 1
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++
+} 
+*/
+
+console.log('vai começar...')
+for (c=1;c<=10;c++) {
+    console.log(c)
+}
+console.log('FIM!')
