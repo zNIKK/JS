36bd4939076c7cd0b5434513361ee3a29fbdf0a9
zNIKK <nicolas.gandolfi11@gmail.com>
2022-12-20 10:58:42 -0300

    files unchanged from other pc

--- JQuery/Filter_Last_First_e_Eq/script/script.js
+++ JQuery/Filter_Last_First_e_Eq/script/script.js
@@ -1,14 +1,14 @@
 let selected = $("#container2");
 
-// console.log(selected).filter(".div2");
+console.log(selected.filter(".div2"));
 
 // console.log(selected).children().filter(".div2");
 
-console.log(selected).children().first(); // last()
+console.log(selected.children().first()); // last()
 
-console.log(selected).children().not(".div2"); // todos os elementos menos esse
+console.log(selected.children().not(".div2")); // todos os elementos menos esse
 
-console.log(selected).children().eq(0); // retorna o indice do array da lista 
+console.log(selected.children().eq(0)); // retorna o indice do array da lista 
 
 
 
